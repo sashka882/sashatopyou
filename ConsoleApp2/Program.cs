16e1342d08f8e5b7d039bf7802dd3a4c3f28d850
mka <mka@A34PC10>
2024-09-17 11:44:55 +0300

    lalala

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,4 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("hvjghggjw")
-    
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//////Console.WriteLine("hvjghggjw");
+
+Console.WriteLine("----------------- weather -----------------");
+Console.Write("Enter curent degree: ");
+int degree = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"{degree*1.8+32}");
